f87cf7ad262ab7eb49c3fc8792eb1221d8dcfdb1
provoloneboy <120835254+provoloneboy@users.noreply.github.com>
2026-02-20 14:31:10 -0500

    Update firebase-config.js with new content on 2026-02-20 19:30:47 UTC.

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Using environment variables for security
 const firebaseConfig = {
-  apiKey: "AIzaSyBRjA6kWFAkugFUrZWF7ojx7fqQAeu8TAA",
-  authDomain: "e-s-recipes.firebaseapp.com",
-  projectId: "e-s-recipes",
-  storageBucket: "e-s-recipes.firebasestorage.app",
-  messagingSenderId: "983679762983",
-  appId: "1:983679762983:web:219412952e4023d3650402",
-  measurementId: "G-T8FMN583SQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
